5eecb589564fd8f751218b654997591f052abab6
HarshitKhurana <hkhurana3@gmail.com>
2019-07-23 18:54:50 +0530

    scope_modifications.pyx added

---
+++ Cython/Examples/Fourth/scope_modifications.pyx
@@ -0,0 +1,24 @@
+
+# Extension Type example
+
+cdef class Student:
+    # cdef block containing attributes of the class.
+    # Note : All attributes must be declared at compile-time only (since it internally uses C-structs)
+    cdef: 
+    # data-type variableName
+        cdef public str _name               # Accessible in python code - readable/writable
+        cdef readonly int _rollNumber       # Accessible in python code - readable only cannot modify
+        str _favSubject
+        int _age
+        
+    # Creating object with the before-mentioned attributes 
+    def __init__ ( self , str name , str favSubject , int age , int rollNumber):
+        self._name = name
+        self._favSubject = favSubject
+        self._age = age
+        self._rollNumber = rollNumber
+
+# In the python code we can access only 2 attributes i.e '_name' and '_rollNumber' with the access level as mentioned above.
+
+# For using this class in `student.py` a simple python module, import as :
+    #   from student cimport student                                                                                                                                                      
